a973d2de23b6c1e493675ff0c673f15fe9989193
AppleEve <etandruszkiewicz@gmail.com>
2022-09-02 15:13:54 +0300

    API integration

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,12 +1,18 @@
+import axios from "axios";
 import React, { useState } from "react";
 import "./DictionarySearch.css";
 
 export default function DictionarySearch() {
   const [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleChange(event) {
