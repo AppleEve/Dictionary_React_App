1d8fd145268111667d523c44fe438cd8111ef724
AppleEve <etandruszkiewicz@gmail.com>
2022-09-03 22:24:52 +0300

    Adding footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,24 @@ function App() {
       <div className="component">
         <header className="App-header">Dictionary</header>
         <DictionarySearch />
+        <footer>
+          The app was coded by{" "}
+          <a
+            href="https://appleeve.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            AppleEve{" "}
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/AppleEve/Dictionary_React_App"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced.
+          </a>
+        </footer>
       </div>
     </div>
   );
