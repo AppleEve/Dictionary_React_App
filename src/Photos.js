0395db21f62df4721154cde808907a9ddaccea0a
AppleEve <etandruszkiewicz@gmail.com>
2022-09-13 21:27:47 +0300

    Update Photos.js
    Changing carousel type

--- src/Photos.js
+++ src/Photos.js
@@ -7,38 +7,10 @@ export default function Photos(props) {
     return (
       <div className="Photos row">
         <div
-          id="carouselExampleIndicators"
-          className="carousel slide"
-          data-bs-ride="true"
+          id="carouselExampleControls"
+          class="carousel slide"
+          data-bs-ride="carousel"
         >
-          <div className="carousel-indicators">
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="0"
-              className="active"
-              aria-current="true"
-              aria-label="Slide 1"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="1"
-              aria-label="Slide 2"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="2"
-              aria-label="Slide 3"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="3"
-              aria-label="Slide 4"
-            ></button>
-          </div>
           <div className="carousel-inner">
             <div className="carousel-item active row">
               <img
@@ -130,28 +102,22 @@ export default function Photos(props) {
             </div>
           </div>
           <button
-            className="carousel-control-prev"
+            class="carousel-control-prev"
             type="button"
-            data-bs-target="#carouselExampleIndicators"
+            data-bs-target="#carouselExampleControls"
             data-bs-slide="prev"
           >
-            <span
-              className="carousel-control-prev-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Previous</span>
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
           </button>
           <button
-            className="carousel-control-next"
+            class="carousel-control-next"
             type="button"
-            data-bs-target="#carouselExampleIndicators"
+            data-bs-target="#carouselExampleControls"
             data-bs-slide="next"
           >
-            <span
-              className="carousel-control-next-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Next</span>
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
           </button>
         </div>
       </div>
