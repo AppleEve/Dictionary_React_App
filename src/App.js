4d85a5f32b6d8d9fb7495d9bd43fe1be67eb6c19
AppleEve <etandruszkiewicz@gmail.com>
2022-09-16 10:11:12 +0300

    Background changes - full screen

--- src/App.js
+++ src/App.js
@@ -1,43 +1,30 @@
 import "./App.css";
 import DictionarySearch from "./DictionarySearch";
-import background from "./background.jpg";
+
 import book from "./books.png";
 
 function App() {
   return (
-    <div
-      className="App"
-      style={{
-        backgroundImage: `url(${background})`,
-        backgroundSize: "cover",
-        height: "100%",
-        backgroundAttachment: "fixed",
-      }}
-    >
+    <div className="App">
       <div className="component">
         <header className="App-header">Dictionary</header>
         <DictionarySearch />
-
-        <footer>
-          The app was coded by{" "}
-          <a
-            href="https://appleeve.netlify.app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            AppleEve{" "}
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/AppleEve/Dictionary_React_App"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced.
-          </a>
-        </footer>
-        <img className="books d-none d-lg-block" src={book} alt="books" />
       </div>
+      <footer>
+        The app was coded by{" "}
+        <a href="https://appleeve.netlify.app" target="_blank" rel="noreferrer">
+          AppleEve{" "}
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/AppleEve/Dictionary_React_App"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced.
+        </a>
+      </footer>
+      <img className="books d-none d-lg-block" src={book} alt="books" />
     </div>
   );
 }
